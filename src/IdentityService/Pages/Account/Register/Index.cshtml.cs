ae16915c2f25f19f9baeafdd35e99035857f80fc
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2026-06-04 12:08:03 +0500

    Sec5. Adding a Sign-Up Page

---
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Account.Register;
+
+// adding headers so that an unregistered user can log in
+[SecurityHeaders]
+[AllowAnonymous]
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
